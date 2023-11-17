193b32826abf6b386322c21699d287e78373d93f
thamste <thamste@gmail.com>
2023-11-17 14:16:28 +0100

    Revert "program cs"
    This reverts commit 1cc8fbec6f507ddf3f2b5cd63a43722297f45a45.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,2 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");

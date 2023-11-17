f965902f4ad1d01a1668c3230d5570f3f6318e48
Kluez <53569718+Kluez@users.noreply.github.com>
2023-11-17 15:18:12 +0100

    345324534

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 for(int i = 1 ; i < 10; i++)
-    Console.WriteLine("Nix tut so intuitiv wie es sollt. Wieso?" + i);
+    Console.WriteLine("Nix tut so intuitiv wie es sollt. Wies 45345o?" + i);
+

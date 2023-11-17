126d59b6c5fc08191b5558d9e3f7c25a5a407a34
Kluez <53569718+Kluez@users.noreply.github.com>
2023-11-17 15:17:27 +0100

    10 plus eins

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 for(int i = 1 ; i < 10; i++)
-    Console.WriteLine("Nix tut so intuitiv wie es sollt. " + i);
+    Console.WriteLine("Nix tut so intuitiv wie es sollt. Wieso?" + i);
